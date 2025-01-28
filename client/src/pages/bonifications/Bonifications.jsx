9c173f987a31d600e7eb36c118cb09c4c21d2a89
Jorge Pascual Fuentecilla <jorgepasf@gmail.com>
2025-01-28 15:43:04 +0100

    avance en sales

--- client/src/pages/bonifications/Bonifications.jsx
+++ client/src/pages/bonifications/Bonifications.jsx
@@ -8,6 +8,16 @@ const Bonifications = () => {
     <Layout title="Bonificaciones">
       <p>Esto es la pagina de bonificaciones</p>
       <DateFilter />
+      <div>
+        <img src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png" alt="Descripción de la imagen" width="200" height="150" />
+
+      </div>
+
+      <div>
+        <img src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png" alt="Descripción de la imagen" width="200" height="150" />
+
+
+      </div>
     </Layout>
   );
 };
