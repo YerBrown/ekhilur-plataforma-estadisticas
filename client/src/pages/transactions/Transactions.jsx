d64d13cdabc5c5b4ad995e867b78a3d1e208b241
samucopp <samucop@icloud.com>
2025-01-29 13:07:21 +0100

    pequeños ajustes en las paginas y en el layout

--- client/src/pages/transactions/Transactions.jsx
+++ client/src/pages/transactions/Transactions.jsx
@@ -3,11 +3,13 @@ import Layout from "../../pages/layout/Layout";
 import TransactionList from "../../components/transactions-list/TransactionsList";
 import mockTransactions from "../../components/transactions-list/mockData.js";
 import SearchBar from "../../components/search-bar/SearchBar";
+import TransactionsFilter from "../../components/buttons/transactions-filter/TransactionsFilter.jsx";
+import "./Transactions.css";
 
 const Transactions = () => {
     const [filteredTransactions, setFilteredTransactions] = useState(mockTransactions);
 
-    const handleSearch = ({term, filter}) => {
+    const handleSearch = ({ term, filter }) => {
         const filtered = mockTransactions.filter((transaction) => {
             if (filter === "date") {
                 return transaction.date.toLowerCase().includes(term);
@@ -21,9 +23,20 @@ const Transactions = () => {
         setFilteredTransactions(filtered);
     };
 
+    const handleFilterChange = (filter) => {
+        if (filter === "Todas") {
+            setFilteredTransactions(mockTransactions);
+        } else if (filter === "Ingresos") {
+            setFilteredTransactions(mockTransactions.filter(transaction => !transaction.amount.toString().includes('-')));
+        } else if (filter === "Gastos") {
+            setFilteredTransactions(mockTransactions.filter(transaction => transaction.amount.toString().includes('-')));
+        }
+    };
+
     return (
         <Layout title="Transacciones">
             <SearchBar onSearch={handleSearch} />
+            <TransactionsFilter onFilterChange={handleFilterChange}/>
             <TransactionList transactions={filteredTransactions} />
         </Layout>
     );
