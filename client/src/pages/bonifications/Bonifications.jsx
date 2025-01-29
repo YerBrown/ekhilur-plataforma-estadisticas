2d5c5d53a75fd47809b42c200a66367a6fa5b571
JorgePascualFuentecilla <jorgepasf@gmail.com>
2025-01-29 20:32:32 +0100

    Css arreglado

--- client/src/pages/bonifications/Bonifications.jsx
+++ client/src/pages/bonifications/Bonifications.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import Layout from "./Layout";
+import Layout from "../layout/Layout";
 import "./Bonifications.css";
+import DateFilter from "../../components/DateFilter/DateFilter";
 
 const Bonifications = () => {
   return (
     <Layout title="Bonificaciones">
-      
+      <DateFilter />
+
+      <div className="bonifications-container">
+        <div className="content-container">
+          
+            <img className="image-container"
+              src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png"
+              alt="Gráfico de barras"
+            />
+          
+          
+            <img className="image-container"
+              src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png"
+              alt="Resumen de transacciones"
+            />
+          
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/sales/Sales.jsx
+++ client/src/pages/sales/Sales.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import Layout from "./Layout";
+import Layout from "../layout/Layout";
 import "./Sales.css";
+import DateFilter from "../../components/DateFilter/DateFilter";
 
-const Sales = () => {
+const Bonifications = () => {
   return (
     <Layout title="Ventas">
-      
+      <DateFilter />
+
+      <div className="sales-container">
+        <div className="content-container">
+          
+            <img className="image-container"
+              src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png"
+              alt="Gráfico de barras"
+            />
+          
+          
+            <img className="image-container"
+              src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png"
+              alt="Resumen de transacciones"
+            />
+          
+        </div>
+      </div>
     </Layout>
   );
-
-
 };
 
-export default Sales;
+export default Bonifications;
