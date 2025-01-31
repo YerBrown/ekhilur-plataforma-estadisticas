cc4a4faf93564e742592013c85964a9e1c36381e
Jorge Pascual Fuentecilla <jorgepasf@gmail.com>
2025-01-31 12:41:30 +0100

    problema de compatibilidad arreglado

--- client/src/pages/bonifications/Bonifications.jsx
+++ client/src/pages/bonifications/Bonifications.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import Layout from "../layout/Layout";
 import "./Bonifications.css";
 import DateFilter from "../../components/DateFilter/DateFilter";
 import TransactionList from "../../components/transactions-list/TransactionsList";
 import mockData from "../../components/transactions-list/mockData.js";
 import BarChartComponent from "../../components/charts/BarChart";
+
+
+
 
 const Bonifications = () => {
   const [bonificationTransactions, setbonificationTransactions] = useState(mockData);
@@ -16,13 +19,6 @@ const Bonifications = () => {
         <div className="content-container">
 
           <BarChartComponent />
-
-
-          <img className="image-container"
-            src="https://python-charts.com/es/parte-todo/grafico-barras-apiladas-matplotlib_files/figure-html/grafico-barras-apiladas-matplotlib.png"
-            alt="Resumen de transacciones"
-          />
-
         </div>
       </div>
       <TransactionList transactions={bonificationTransactions} />
