238385858f78f34922e90e4d81d92768136910d5
samucopp <samucop@icloud.com>
2025-02-03 13:33:54 +0100

    modificacion del controlador de autentificacion para guardar el role y el username. Modificaciones del los endpoints para que se hagan en base al username que se logea. Rutas aseguradas para que solo puedan acceder usuarios logeados

--- server/database/mongo-init/1.init-users.js
+++ server/database/mongo-init/1.init-users.js
@@ -5,6 +5,7 @@ db.users.insertMany([
     {
         _id: ObjectId("507f1f77bcf86cd799439011"),
         username: "Alomorga",
+        email: "alomorga@example.com",
         password:
             "$2a$10$80u1oHEBZ46kE3WVAObuH.udCnl6BsR1iM2RpLee56tFDq/fO.E5S",
         role: "user",
@@ -12,6 +13,7 @@ db.users.insertMany([
     {
         _id: ObjectId("507f1f77bcf86cd799439012"),
         username: "ilandatxe",
+        email: "ilandatxe@example.com",
         password:
             "$2a$10$80u1oHEBZ46kE3WVAObuH.udCnl6BsR1iM2RpLee56tFDq/fO.E5S",
         role: "user",
@@ -19,6 +21,7 @@ db.users.insertMany([
     {
         _id: ObjectId("507f1f77bcf86cd799439012"),
         username: "FotosTorres",
+        email: "fotostorres@example.com",
         password:
             "$2a$10$80u1oHEBZ46kE3WVAObuH.udCnl6BsR1iM2RpLee56tFDq/fO.E5S",
         role: "commerce",
